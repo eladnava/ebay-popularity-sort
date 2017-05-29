431618fd1ce89c5e881ff2e84f1d415062973f00
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-05-29 10:16:40 +0300

    Not touch items without sales

--- assets/js/content.js
+++ assets/js/content.js
@@ -13,10 +13,11 @@ $(document).ready(function () {
         var results = [];
 
         // Traverse search results
-        $('li[listingid]').each(function () {
+        $('li[listingid]:has(.hotness-signal)').each(function () {
             // Convert to jQuery object
             var listing = $(this);
 
+            if (listing.find('.hotness-signal:contains(" sold")').length) {
             // Default listing sold count to 0
             var soldCount = 0;
 
@@ -28,6 +29,7 @@ $(document).ready(function () {
 
             // Delete element temporarily
             listing.remove();
+            }
         });
 
         // Sort all listings by sold count DESC
@@ -38,13 +40,13 @@ $(document).ready(function () {
         // Get search results parent list
         var ul = $('ul#ListViewInner');
 
-        // Warn the user about the modified result order
-        ul.append('<li>These search results have been modified by <b>eBay™ Popularity Sort</b>.</li>');
-        ul.append('<li><hr /></li>');
-
         // Re-add the sorted results
-        results.forEach(function (item) {
-            ul.append(item.listing);
-        });
+        ul.prepend(results.map(function(item){
+            return item.listing;
+        }));
+
+        // Warn the user about the modified result order
+        ul.prepend('<li><hr /></li>');
+        ul.prepend('<li>These search results have been modified by <b>eBay™ Popularity Sort</b>.</li>');
     });
 });
